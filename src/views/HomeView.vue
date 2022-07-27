29cfb42a236594f5c29e439bc702e311d52eccb0
Horik <hr.mail.qaq@gmail.com>
2022-07-28 06:09:14 +0800

    [test] 本地前端和后端服务器的交互测试通过

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Test CICD" />
     <div>
-      <button @click="test"> test </button>
+      <button @click="testregister"> testregister </button>
+      <button @click="testlogin"> testlogin </button>
+      <button @click="testfindall"> testfindall </button>
     </div>
   </div>
 </template>
@@ -18,14 +20,38 @@ export default {
     HelloWorld
   },
   methods: {
-    test() {
-      console.log('test')
+    testregister() {
+      console.log('testregister')
       this.$axios.post(
         'user/register',
         this.$qs.stringify({
           username: 'testAxios',
           password: 'test'
         })
+      ).then(response => {
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    testlogin() {
+      console.log('ttestloginest')
+      this.$axios.post(
+        'user/login',
+        this.$qs.stringify({
+          username: 'testAxios',
+          password: 'test'
+        })
+      ).then(response => {
+        console.log(response)
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    testfindall() {
+      console.log('test')
+      this.$axios.get(
+        'user/find_all',
       ).then(response => {
         console.log(response)
       }).catch(error => {
