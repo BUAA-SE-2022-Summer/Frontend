c6698ec6c1eaf1590436de815acf57f90277b272
Horik <hr.mail.qaq@gmail.com>
2022-07-27 21:52:54 +0800

    [impl] dev和production环境都在域名的子目录下启动

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/app/web/',  // /app/web/ 对应后面`nginx`路径，这里添加的目的是其他静态资源文件统一前缀路径
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/app/web/'   // /app/web/ 对应后面`nginx`路径，这里添加的目的是其他静态资源文件统一前缀路径
+  //   : '/',
 })
