18b014b08d265b6609ed601d9fe9ad853ef28ce5
zbh6666 <bohao2020@163.com>
2022-07-31 19:36:42 +0800

    分支合并尝试

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <div class="about">
     <h1>解决了nginx部署后 vue非主页路由刷新后出现404的问题</h1>
     <h1>试试push上去能不能直接改变 try</h1>
+    <h1>分支合并尝试</h1>
     <div class="mt-3">
     <b-button-group>
       <b-button variant="success">Success</b-button>
