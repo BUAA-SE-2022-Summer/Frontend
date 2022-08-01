b7ac486796196f8f6104e5517f9cf6127e5da144
zbh6666 <bohao2020@163.com>
2022-08-01 19:39:43 +0800

    [impl]:产品功能介绍弹窗修改

--- src/views/Welcome_page.vue
+++ src/views/Welcome_page.vue
@@ -1,26 +1,19 @@
 <template>
   <div class="main">
     <!--<div class="top" style="position: absolute;top:20px;left:20px;font-size: 30px"><b>欢迎使用墨书</b></div>-->
-    <div style="background-color: #999999;position: absolute;width:1500px;top:20px;height:130px;left:10px">
+    <div style="background-color: #999999;position: absolute;width:1520px;top:0px;height:130px;left:0px">
     <div class="mobook_head">
       <img src="../img/墨书头.png" style="position: absolute;top:20px;left:70px;width: 60px;height: 60px;border-radius: 50%;border-color: white;border-width: 1px;margin-right: 50px;margin-top: 10px">
     </div>
     <div class="name" style="position: absolute;top:50px;left:150px;font-size: 20px"><b>墨书</b></div>
       <div class="产品介绍" style="position: absolute;top:50px;left:300px;font-size: 20px;background-color: #999999">
-        <el-popover
-            placement="top-start"
-            title="墨书产品功能介绍"
-            width="200"
-            trigger="hover"
-            content="墨书是一款方便的协作设计软件">
-          <el-button slot="reference" style="background-color: #999999;border-color: #999999"><b>产品功能介绍</b></el-button>
-        </el-popover>
+          <el-button slot="reference" style="background-color: #999999;border-color: #999999" @click="clickintroduction"><b>产品功能介绍</b></el-button>
       </div>
     <el-button v-if="this.iflogin===0" type="info" round style="background-color: #99a9bf;position: absolute;left:1300px;top:50px" @click="login">登录/注册</el-button>
     <img v-if="this.iflogin===1" :src="this.userhead" style="position: absolute;top:30px;left:1300px;width: 50px;height: 50px;border-radius: 50%;border-color: white;border-width: 1px;margin-right: 50px;margin-top: 10px">
     <div v-if="this.iflogin===1" style="position: absolute;top:55px;left:1360px"><b>{{this.username}}</b></div>
     </div>
-    <div class="zouma" style="position: absolute;top:150px;left:10px;width:1500px;background-color: whitesmoke;height:600px">
+    <div class="zouma" style="position: absolute;top:130px;left:0px;width:1520px;background-color: whitesmoke;height:350px;z-index: 1">
   <el-carousel indicator-position="outside" height="500px">
     <el-carousel-item>
       <img src="../img/走马灯1.jpg" width="100%" height="100%">
@@ -32,6 +25,32 @@
       <div class="introduct" style="font-size: 30px;position: absolute;left:150px"><b>墨书——让产品设计更优雅</b></div>
       <div><el-button type="info" style="position: absolute;height: 50px;width:100px;left:1200px" @click="freeuse">免费使用</el-button></div>
     </div>
+    <div v-if="this.click_introduction===1" class="introduction_card" style="position: absolute;top:100px;left:210px;width: 600px;height:300px;z-index: 2">
+      <el-card class="box-card" style="position: absolute;height:300px">
+        <div slot="header" class="clearfix">
+          <span><b>墨书功能介绍</b></span>
+        </div>
+        <div>
+          作为一款简洁高效的在线合作办公设计软件，墨书包含以下功能
+        </div>
+        <div>
+          <img src="../img/项目管理.jpg" style="width: 100px;height: 80px;left:20px;position: absolute">
+        </div>
+        <div style="position: absolute;top:130px;left:130px"><b>项目管理</b></div>
+        <div>
+          <img src="../img/原型设计.jpg" style="width: 100px;height: 80px;left:240px;position: absolute">
+        </div>
+        <div style="position: absolute;top:130px;left:360px"><b>原型设计</b></div>
+        <div>
+          <img src="../img/uml.jpg" style="width: 100px;height: 80px;left:20px;position: absolute;top:200px">
+        </div>
+        <div style="position: absolute;top:220px;left:130px"><b>uml图绘制</b></div>
+        <div>
+          <img src="../img/文档编辑.jpg" style="width: 100px;height: 80px;left:240px;position: absolute;top:200px">
+        </div>
+        <div style="position: absolute;top:220px;left:360px"><b>文档编辑</b></div>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -42,8 +61,8 @@ export default {
     return{
       iflogin:1,
       username:'张博皓',
-      userhead: 'https://xuemolan.oss-cn-hangzhou.aliyuncs.com/test001.jpg'
-
+      userhead: 'https://xuemolan.oss-cn-hangzhou.aliyuncs.com/test001.jpg',
+      click_introduction:0
     }
   },
   methods:{
@@ -63,6 +82,14 @@ export default {
     },
     login(){
       this.$router.push('/login')
+    },
+    clickintroduction(){
+      if(this.click_introduction===0){
+        this.click_introduction=1;
+      }
+      else{
+        this.click_introduction=0;
+      }
     }
   }
 }
