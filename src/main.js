989b4fa02ca94938436dc74138abc5d63adf3799
Horik <hr.mail.qaq@gmail.com>
2022-08-03 11:00:14 +0800

    [chore] 修改依赖配置文件

--- src/main.js
+++ src/main.js
@@ -4,23 +4,34 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 import qs from 'qs'
+
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import '@/custom-component' // 注册自定义组件
+
+// 引入全局样式
+import '@/assets/iconfont/iconfont.css'
+import '@/styles/animate.scss'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/styles/reset.css'
+import '@/styles/global.scss'
 
 axios.defaults.baseURL = '/api'
 Vue.prototype.$axios = axios;
 Vue.prototype.$qs = qs
 
 Vue.config.productionTip = false
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 Vue.use(ElementUI);
+
 new Vue({
   router,
   store,
