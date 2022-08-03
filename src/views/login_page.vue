8e24129585c7e6d88409caedfb4551cffeeee70f
Ivine11 <i2506851474@163.com>
2022-08-04 01:09:48 +0800

    [patch]登录注册页面ui修改完成

--- src/views/login_page.vue
+++ src/views/login_page.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="div_1">
+    <v-app>
     <div style="width:100%;" class="main">
     <div class="left" style="position:fixed;float:left;width: 800px;height: 100vh;margin-top: 0;">
     <div class="left-top" style="width:100%;height:30vh;">
@@ -9,39 +10,62 @@
       
     </div>
     <div class="left-bottom" style="width:100%;height:65vh;">
-       <v-card class="mx-auto my-12" height="50vh" width="30vw">
+       <v-card class="mx-auto my-12"  width="30vw">
           <v-img src="../assets/card2.png" style="width: 30vw;"></v-img>
       </v-card>
       
     </div>
     </div>
-    <div class="right" style="position:fixed;float:left;margin-left: 800px;height:100vh;width: 800px;margin-top: 5vh;">
-    <div class="right-top" style="width:100%;height:55vh;">
-      <v-card class="mx-auto my-12" width="25vw" height="50vh" style="float:left;background-color: white;">
-          <div style="width:100%;margin-top: 3vh;">
-            <img src="../img/login.png" style="width:22vw;margin-left: 1vw;">
+    <div class="right" style="position:fixed;float:left;margin-left: 800px;height:100vh;width: 800px;margin-top: 4px;">
+    <div class="right-top" style="width:100%;height:550px;">
+      <v-card class="mx-auto my-12" width="400px" height="500px" style="float:left;background-color: white;">
+          <div style="width:100%;margin-top: 0;">
+            <img src="../img/login.png" style="width:350px;margin-left: 25px;">
           </div>
-          <div style="width:21vw;margin-left:2vw;margin-top: 1vh;">
+          <div style="width:300px;margin-left:50px;margin-top: 10px;">
             <v-text-field v-model="form.username" label="昵称" outlined clearable></v-text-field>
             <v-text-field v-model="form.password" label="密码" outlined clearable></v-text-field>
-            <v-btn x-large dark @click="login">
-              登录
-            </v-btn>
-            <v-btn x-large dark @click="toRegister">
-              注册
-            </v-btn>
+           <!-- <v-app id="inspire"> -->
+            <v-bottom-navigation
+              :value="value"
+              color="pink"
+            >
+              <v-btn @click="login">
+                <span>登录</span>
+          
+                <v-icon>mdi-login</v-icon>
+              </v-btn>
+          
+              <v-btn @click="toRegister">
+                <span>注册</span>
+          
+                <v-icon>mdi-account-plus</v-icon>
+              </v-btn>
+          
+              <v-btn @click="toMain">
+                <span>返回主页</span>
+          
+                <v-icon>mdi-arrow-left</v-icon>
+              </v-btn>
+            </v-bottom-navigation>
+            <!-- </v-app> -->
           </div>
         </v-card>
+        
     </div>
 
-    <div class="right-bottom" style="width:100%;height:30vh;">
-        <v-card class="mx-auto my-12" width="450" style="float:left">
+    <div class="right-bottom" style="width:25vw;height:400px; margin-left: 0;">
+    <v-card class="mx-auto my-12" style="margin-left:0;width: 25vw;">
+         <v-img src="../assets/card3.png" style="margin-top:0;width: 25vw;"></v-img>
+    </v-card>
+        <!-- <v-card class="mx-auto my-12" width="250px" style="float:left;margin-left: 0;">
           <v-img src="../assets/card3.png" style="margin-top:0"></v-img>
-      </v-card>
+      </v-card> -->
       
     </div>
     </div>
     </div>
+    </v-app>
   </div>
 </template>
 
@@ -54,7 +78,8 @@ export default {
       form: {
         username: '',
         password: '',
-      }
+      },
+      value:1,
     }
   },
   methods: {
@@ -94,6 +119,12 @@ export default {
     toRegister: function () {
       // 跳转注册的路由
       this.$router.push('/register');
+
+    },
+    toMain: function () {
+      // 跳转注册的路由
+      this.$router.push('/app/web');
+
     }
   }
 }
@@ -172,7 +203,7 @@ export default {
 
 
 .main{
-  width: 100vw;
+  width: 100%;
   height: 100vh;
   background-image: url("../assets/welBack2.jpg");
   background-size:cover ;
