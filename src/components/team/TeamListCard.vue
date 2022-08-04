9521c8a12c1722b9029af8e9cb70925f18935f7e
Horik <hr.mail.qaq@gmail.com>
2022-08-05 03:11:41 +0800

    [fix] 切换团队页面的显示

--- src/components/team/TeamListCard.vue
+++ src/components/team/TeamListCard.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card max-height="28.7vh" min-height="28.7vh" min-width="45vh" max-width="45vh" class="mx-auto" tile>
-    <v-list v-if="items.length() !== 0">
+    <v-list v-if="items">
       <v-list-item two-line v-for="(item, i) in items" :key="i" @click="changeCurrentTeamID(item)">
         <v-list-item-icon>
           <Avatar :username="items.username" :backgroundcolor="items.team_name" color="#fff"
