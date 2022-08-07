e90cf6fc63397412c3e9f1c6316fbed66c267f86
Ivine11 <i2506851474@163.com>
2022-08-07 23:21:37 +0800

    项目层修改

---
+++ src/views/test.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div class="top">
+
+  </div>
+  <div>
+    <div style="float:left">
+      <project-menu></project-menu>
+    </div>
+    <div style="float:left">
+
+    </div>
+  </div>
+</div>
+</template>
+<script>
+import projectMenuVue from "../components/project/projectMenu.vue";
+import projectAllVue from "../components/project/projectAll.vue";
+
+</script>
