f053ee6718054a8cd787b616d5d8e5d98f720423
Ivine11 <i2506851474@163.com>
2022-08-07 11:50:58 +0800

    项目相关功能对接

---
+++ src/components/project/projectMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+  <div>
+    <v-card
+      max-width="300"
+      class="mx-auto"
+      
+    >
+  
+
+      <v-app-bar
+        dark
+        color=""
+        style="background:#508090;"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+  
+        <v-toolbar-title>菜单栏</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn style="background-color: transparent;">
+          搜索项目
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-app-bar>
+  
+      <v-container>
+         <v-btn style="width: 100%;font-size: 20px;height: 70px;background-color: #ccbfd2;">
+         <v-icon>mdi-newspaper-plus</v-icon>
+          新建项目
+         </v-btn >
+         <v-btn style="width: 100%;font-size: 20px;height: 70px;background-color: #8da8c4;">
+         <v-icon>mdi-all-inclusive</v-icon>
+          全部项目
+         </v-btn>
+         <v-btn style="width: 100%;font-size: 20px;height: 70px;background-color: #ffe8f8;">
+         <v-icon>mdi-account-details</v-icon>
+          我的创建
+         </v-btn>
+         <v-btn style="width: 100%;font-size: 20px;height: 70px;background-color: #c0dbf8;">
+         <v-icon>mdi-star-box</v-icon>
+          我的收藏
+         </v-btn>
+         <v-btn style="width: 100%;font-size: 20px;height: 70px;background-color: #dfe0ff;">
+         <v-icon>mdi-delete</v-icon>
+          回收站
+         </v-btn>
+      </v-container>
+    </v-card>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,283 +1,235 @@
 <template>
-  <div class="home">
-<div style="margin-top:100px;width:600px;margin-left: 200px;">
-   <v-data-table
-      :headers="headers"
-      :items="desserts"
-      sort-by="projectUser"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <v-toolbar-title>My Projects</v-toolbar-title>
-          <v-divider
-            class="mx-4"
-            inset
-            vertical
-          ></v-divider>
-          <v-spacer></v-spacer>
-          <v-dialog v-model="dialog" max-width="500px">
- 
-            <v-card>
-              <v-card-title>
-                <span class="headline">修改项目名称</span>
-              </v-card-title>
   
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12" sm="6" md="4">
-                      <v-text-field v-model="editedItem.projectName" label="Dessert projectName"></v-text-field>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="save">Save</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
 
-          <v-dialog v-model="dialog2" max-width="500px">
-
-            <v-card>
-              <v-card-title>
-                <span class="headline">删除项目</span>
-              </v-card-title>
-  
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-                <v-btn color="blue darken-1" text @click="OK">OK</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.actions="{ item }">
-        <v-icon
-          small
-          class="mr-2"
-          @click="editItem(item)"
-        >
-          mdi-pencil
-        </v-icon>
-        <v-icon
-          small
-          @click="deleteItem(item)"
-        >
-          mdi-delete
-        </v-icon>
-      </template>
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
-      </template>
-    </v-data-table>
-    </div>
-  </div>
 </template>
-
 <script>
-
-import qs from "qs";
-
+import qs from 'qs'
 export default {
-  projectName: 'Home',
-  data() {
-    return {
-      admins: [
-        ['成员管理', 'mdi-account-cog'],
-        ['操作日志', 'mdi-book-open-outline'],
-      ],
-      numproject:0,
-      projectlist:[],
-      teamid:0,
-
-      dialog: false,
-      dialog2: false,
-    headers: [
-      {
-        text: '项目名称',
-        align: 'start',
-        sortable: false,
-        value: 'projectName',
-      },
-      { text: 'projectUser', value: 'projectUser' },
-      { text: 'projectID ', value: 'projectID' },
-      { text: 'projectTime ', value: 'projectTime' },
-      { text: 'is_star ', value: 'is_star' },
-      { text: 'Actions', value: 'actions', sortable: false },
-      ],
-      desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      projectName: '',
-      projectUser: 0,
-      projectID: 0,
-      projectTime: 0,
-      is_star: 0,
-    },
-    defaultItem: {
-      projectName: '',
-      projectUser: 0,
-      projectID: 0,
-      projectTime: 0,
-      is_star: 0,
-    },
-    }
-
-  },
-    computed: {
-    formTitle () {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    },
-  },
-
-  watch: {
-    dialog (val) {
-      val || this.close()
-    },
-  },
-  created() {
-    this.teamid=sessionStorage.getItem('TeamID');
-    
+  created(){
+    // this.star_project(1)
+    // this.unstar_project(1)
+    // this.create_project("first")
+    // this.rename_project(1, "First")
+    // this.get_star_project_list()
+    // this.get_create_project_list()
+    // this.delete_project(1)
+    // this.get_delete_project_list()
   },
   methods:{
-    findproject(row,column,cell,event){
-       console.log(row.projectID);
-       console.log(row.project_root_fileID);
-       sessionStorage.setItem('ProjectID',JSON.stringify(row.projectID));
-       sessionStorage.setItem('project_root_fileID',JSON.stringify(row.project_root_fileID));
-       this.$router.push('/dashboard/demo/console');
-    },
-    initialize () {
-      // this.desserts = [
-      //   {
-      //     projectName: 'Frozen Yogurt',
-      //     projectUser: 159,
-      //     projectID: 6.0,
-      //     projectTime: 24,
-      //     is_star: 4.0,
-      //   },
-       
-      // ]
+    //收藏功能,通过测试
+    star_project(ID){
+      var teamID=sessionStorage.getItem('TeamID');
+      // console.log(teamID)
       this.$axios({
-       method: 'post',
-       url: '/project/get_project_list',
-       data: qs.stringify({
-         teamID:this.teamid
-       })
-     })
-         .then(res => {
-           console.log(res.data)
-           if (res.data.errno === 0) {
-              this.$message.success("获取项目列表成功");
-              this.desserts=res.data.project_list
-              console.log(this.desserts)
-            } else {
-             alert(res.data.msg);
-              this.$message.error(res.data.msg);
-            }
-         })
-         .catch(err => {
-           console.log(err);         /* 若出现异常则在终端输出相关信息 */
-         })
-    },
-
-    editItem (item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
-    },
-
-    deleteItem (item) {
-      const index = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog2=true
-      // confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
-    },
-    OK(){
-      // this.desserts.splice(index, 1)
-      console.log("删除", this.editedItem)
-      this.delete_project(this.editedItem.projectID)
-      this.close()
-    },
-    delete_project(ID){
-      this.$axios({
-       method: 'post',
-       url: '/project/delete_project',
-       data: qs.stringify({
-        projectID:ID,
-         teamID:this.teamid,
-
-       })
-     })
-         .then(res => {
-           console.log(res.data)
-           if (res.data.errno === 0) {
-              this.$message.success("成功");
-              this.initialize()
-            } else {
-             alert(res.data.msg);
-              this.$message.error(res.data.msg);
-            }
-         })
-         .catch(err => {
-           console.log(err);         /* 若出现异常则在终端输出相关信息 */
-         })
-    },
-    close () {
-      this.dialog = false
-      this.dialog2 = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
+        method: 'post',
+        url: '/project/star_project',
+        data: qs.stringify({
+          projectID:ID,
+          teamID: teamID
+          // teamID: 1
+        })
       })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            this.$message.success("收藏项目成功");
+          } else {
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
     },
-    rename_project(ID, Name){
-      console.log("修改项目名称", ID, Name)
+    //取消收藏,通过测试
+    unstar_project(ID){
+      var teamID=sessionStorage.getItem('TeamID');
       this.$axios({
-       method: 'post',
-       url: '/project/rename_project',
-       data: qs.stringify({
-        projectID:ID,
-         teamID:this.teamid,
-         project_name:Name
+        method: 'post',
+        url: '/project/unstar_project',
+        data: qs.stringify({
+          projectID:ID,
+          teamID: teamID
+          // teamID:1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            this.$message.success("取消收藏项目成功");
+          } else {
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //创建项目，通过测试
+    create_project(name){
+      var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/create_project',
+        data: qs.stringify({
+          project_name:name,
+          teamID: teamID
+          // teamID: 1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            this.$message.success("取消收藏项目成功");
+          } else {
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //删除项目,通过测试
+    delete_project(ID) {
+      var teamID = sessionStorage.getItem('TeamID')
+      this.$axios({
+        method: 'post',
+        url: '/project/delete_project',
+        data: qs.stringify({
 
-       })
-     })
-         .then(res => {
-           console.log(res.data)
-           if (res.data.errno === 0) {
-              this.$message.success("成功");
-            } else {
-             alert(res.data.msg);
-              this.$message.error(res.data.msg);
-            }
-         })
-         .catch(err => {
-           console.log(err);         /* 若出现异常则在终端输出相关信息 */
-         })
+          projectID: ID,
+          // teamID: teamID,
+          teamID: 1
+
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            this.$message.success("成功");
+            // this.initialize()
+          } else {
+            // alert(res.data.msg);
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //重命名项目,通过测试
+    rename_project(ID, Name) {
+      console.log("修改项目名称", ID, Name)
+      var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/rename_project',
+        data: qs.stringify({
+          projectID: ID,
+          // teamID: teamID,
+          teamID: 1,
+          project_name: Name
+
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            this.$message.success("成功");
+          } else {
+            // alert(res.data.msg);
+            this.$message.error(res.data.msg);
+          }
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //查看改团队项目,通过测试
+    get_star_project_list(){
+      var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/get_star_project_list',
+        data: qs.stringify({
+          // teamID: teamID
+          teamID: 1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            console.log(res.data)
+          } 
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //查看用户创建项目,通过测试
+    get_create_project_list(){
+      var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/get_create_project_list',
+        data: qs.stringify({
+          teamID: teamID
+          // teamID: 1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            console.log(res.data)
+          } 
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
+    },
+    //查看回收站项目,通过测试
+    get_delete_project_list(){
+      var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/get_delete_project_list',
+        data: qs.stringify({
+          teamID: teamID
+          // teamID: 1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            console.log(res.data)
+          } 
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
     },
 
-    save () {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-        this.rename_project(this.editedItem.projectID, this.editedItem.projectName)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
-    },
-  
+  },
+  //回收站彻底删除项目
+  delete_project_recycle_bin(ID){
+    var teamID=sessionStorage.getItem('TeamID');
+      this.$axios({
+        method: 'post',
+        url: '/project/get_delete_project_list',
+        data: qs.stringify({
+          teamID: teamID
+          // teamID: 1
+        })
+      })
+        .then(res => {
+          console.log(res.data)
+          if (res.data.errno === 0) {
+            console.log(res.data)
+          } 
+        })
+        .catch(err => {
+          console.log(err);         /* 若出现异常则在终端输出相关信息 */
+        })
   }
 }
 </script>
-
-
-
-
-
