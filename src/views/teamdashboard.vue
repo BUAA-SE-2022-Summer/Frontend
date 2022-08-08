d4243d8b7cdc85686b57b817b1ddc22cf9f9c898
jiqinchun <2834481349@qq.com>
2022-08-08 23:10:15 +0800

    [impl]:团队界面新建团队;项目层面切换团队;[fix]:创建团队后新建团队无法在团队界面实时显示的bug

--- src/views/teamdashboard.vue
+++ src/views/teamdashboard.vue
@@ -27,20 +27,20 @@
                     <v-list dense>
                       <v-list-item v-for="(key, index) in filteredKeys" :key="index">
                         <v-list-item-content style="padding-left: 35px">
-                          {{ key }}:
+                          {{ key}}:
                         </v-list-item-content>
                         <v-list-item-content>
-                          {{ item[key.toLowerCase()] }}
+                          {{item[key.toLowerCase()] }}
                         </v-list-item-content>
                       </v-list-item>
                     </v-list>
-                    <v-card-actions class="text-h6 white--text" style="height: 55px; display: flex;">
+                    <v-card-actions class="text-h6 white--text" style="height: 55px;display: flex;">
                       <v-btn :class="{ 'show-btns': hover }" depressed outlined color="primary"
-                        style="border-radius: 4px;height: 35px;" @click="goTeam(item)">
+                             style="border-radius: 4px;height: 35px;" @click="goTeam(item)">
                         进入团队
                       </v-btn>
                       <v-btn :class="{ 'show-btns': hover }" depressed outlined style="border-radius: 4px;height: 35px;margin-left: auto;
- order:2" @click="exitProject(project.id)">
+ order:2" @click="queryTeam(item)">
                         查看团队信息
                       </v-btn>
                     </v-card-actions>
@@ -53,16 +53,72 @@
       </v-container>
       <v-container>
         <v-data-iterator :items="teams" :items-per-page.sync="itemsPerPage" :page.sync="page" :search="search"
-          :sort-by="sortBy.toLowerCase()" :sort-desc="sortDesc" hide-default-footer>
+                         :sort-by="sortBy.toLowerCase()" :sort-desc="sortDesc" hide-default-footer>
           <template v-slot:header>
+            <v-dialog v-model="dialogAdd" max-width="500px">
+              <v-card>
+                <v-card-title class="text-h5">新建团队</v-card-title>
+                <v-col>
+                  <v-text-field label="项目名称" v-model="addName"></v-text-field>
+                </v-col>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="close">取消</v-btn>
+                  <v-btn color="blue darken-1" text @click="createTeam">创建</v-btn>
+                  <v-spacer></v-spacer>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <!--查询团队信息的界面-->
+            <v-dialog v-model="dialogLook" width="500px">
+              <v-card>
+                <v-card-title>
+                  <span class="headline">团队信息</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="6" lg="6">
+                        <v-text-field
+                            v-model="teamName"
+                            label="团队名称"
+                            required
+                            readonly
+                            style="border-radius: 4px;height: 35px;"
+                        ></v-text-field>
+                      </v-col>
+                      <v-col cols="12" sm="6" md="6" lg="6">
+                        <v-text-field
+                            v-model="teamIntroduce"
+                            label="团队介绍"
+                            required
+                            readonly
+                            style="border-radius: 4px;height: 35px;"
+                        ></v-text-field>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" depressed outlined flat @click="dialogLookOK">
+                    关闭
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
             <v-toolbar dark color="blue darken-3" class="mb-1" style="width: 1280px;height: 58px">
               <span style="float:left;">全部团队</span>
               <v-text-field v-model="search" clearable flat solo-inverted hide-details prepend-inner-icon="mdi-magnify"
-                label="Search" style="position: absolute;left:125px;width:200px;"></v-text-field>
+                            label="Search" style="position: absolute;left:550px;width:200px;"></v-text-field>
               <template v-if="$vuetify.breakpoint.mdAndUp">
                 <v-select v-model="sortBy" flat solo-inverted hide-details :items="keys"
-                  prepend-inner-icon="mdi-magnify" label="Sort by" style="position: absolute;left:350px;width:200px;">
+                          prepend-inner-icon="mdi-magnify" label="Sort by" style="position: absolute;left:800px;width:200px;">
                 </v-select>
+                <v-btn depressed color="blue" style="position: absolute;left:120px;" @click="logcreate">
+                  <v-icon>mdi-plus</v-icon>
+                  <span>新建团队</span>
+                </v-btn>
                 <v-divider style="margin: 1px"></v-divider>
                 <v-btn-toggle v-model="sortDesc" mandatory style="float:right;">
                   <v-btn depressed color="blue" :value="false">
@@ -77,29 +133,6 @@
           </template>
           <template v-slot:default="props">
             <v-row>
-              <!-- <v-col>
-                <v-hover v-slot="{ hover }">
-                  <v-card :elevation="hover ? 15 : 2" height="252px" width="280px" style="border-radius: 10px;">
-                    <v-img style="border-radius: 10px;">
-                      <v-card-title class="text-h6" style="padding-top: 2px;padding-bottom: 2px">
-                        <v-row class="fill-height flex-column" justify="space-between">
-                          <p class="mt-4 subheading text-center">
-                            新建团队
-                          </p>
-                        </v-row>
-                      </v-card-title>
-                      <v-card-actions class="text-h6" style="padding-left:87px">
-                        <v-btn class="text-h6" style="width:100px;height: 100px;" @click="newTeam">
-                          <v-icon large class="mr-2" style="font-size: 10px">
-                            mdi-plus
-                          </v-icon>
-                        </v-btn>
-                      </v-card-actions>
-                      <v-card-text class="text-center" style="padding: 2px">点击上方新建按钮创建团队</v-card-text>
-                    </v-img>
-                  </v-card>
-                </v-hover>
-              </v-col> -->
               <v-col v-for="item in props.items" :key="item.name" cols="12" sm="6" md="3" lg="3">
                 <v-hover v-slot="{ hover }">
                   <v-card :elevation="hover ? 15 : 2" style="border-radius: 10px;width: 280px">
@@ -123,13 +156,13 @@
                         </v-list-item-content>
                       </v-list-item>
                     </v-list>
-                    <v-card-actions class="text-h6 white--text" style="height: 55px; display: flex;">
+                    <v-card-actions class="text-h6 white--text" style="height: 55px;display: flex;">
                       <v-btn :class="{ 'show-btns': hover }" depressed outlined color="primary"
-                        style="border-radius: 4px;height: 35px;" @click="goTeam(item)">
+                             style="border-radius: 4px;height: 35px;" @click="goTeam(item)">
                         进入团队
                       </v-btn>
                       <v-btn :class="{ 'show-btns': hover }" depressed outlined style="border-radius: 4px;height: 35px;margin-left: auto;
- order:2" @click="exitProject(project.id)">
+ order:2" @click="queryTeam(item)">
                         查看团队信息
                       </v-btn>
                     </v-card-actions>
@@ -162,6 +195,7 @@
 </template>
 <script>
 import Workspacebanner from "@/views/Workspacebanner";
+import qs from "qs";
 export default {
   data() {
     return {
@@ -171,6 +205,15 @@ export default {
       page: 1,
       itemsPerPage: 4,
       sortBy: 'name',
+
+      dialogAdd: false,
+      addName: '',
+
+      dialogLook: false,
+      teamName: '',
+      teamIntroduce: '',
+
+
       keys: [
         'team_name',
         'team_manager',
@@ -191,6 +234,11 @@ export default {
       return this.keys.filter(key => key !== 'Name')
     },
   },
+  watch: {
+    dialogAdd(val) {
+      val || this.close()
+    },
+  },
   methods: {
     nextPage() {
       if (this.page + 1 <= this.numberOfPages) this.page += 1
@@ -201,13 +249,65 @@ export default {
     updateItemsPerPage(number) {
       this.itemsPerPage = number
     },
+    close() {
+      this.dialogAdd = false
+    },
+    logcreate () {
+      this.dialogAdd = true;
+    },
+    createTeam() {
+      this.$axios({
+        method: 'post',
+        url: '/api/team/create_team',
+        data: this.$qs.stringify({
+              team_name: this.addName,
+            }
+        )
+      })
+          .then(res => {
+            // this.teams.push({
+            //   teamID: res.data.teamID,
+            //   team_name: this.addName,
+            //   create_time: res.data.create_time,
+            //   creator: res.data.creator,
+            // })
+            this.$axios.post(
+                '/api/team/show_my_team_list'
+            ).then(res => {
+              this.teams = res.data.team_list;
+              // 获取team_list的最近团队4个
+              this.recentTeams = res.data.team_list.slice(0, 4);
+            })
+                .catch(err => {
+                  console.log(err);
+                });
+            if (res.data.errno === 0) {
+              this.$message.success("创建团队成功");
+            } else {
+              this.$message.error("创建团队失败");
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      this.close();
+      this.addName = '';
+    },
     goTeam(item) {
       sessionStorage.setItem('TeamID', JSON.stringify(item.teamID));
       console.log("选择的团队id为: " + item.teamID);
       sessionStorage.setItem('TeamName', JSON.stringify(item.team_name));
       console.log("选择的团队名为: " + item.team_name);
       this.$router.push('/projectdashboard');
-    }
+    },
+    queryTeam(item) {
+      this.dialogLook = true;
+      this.teamName = item.team_name;
+    },
+    dialogLookOK() {
+      this.dialogLook = false;
+      this.teamName = '';
+    },
   },
   created() {
     this.$axios.post(
